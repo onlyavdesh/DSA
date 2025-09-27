09e71ebc3f8675e19a8af09d6cd937267db6135d
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2025-09-27 12:21:54 +0530

    here is new code

--- functionsq.java
+++ functionsq.java
@@ -1,35 +1,34 @@
-// public class functionsq {
+public class functionsq {
 
-//     public static int[] change(int a, int b) {
-//         int temp = a;
-//         a = b;
-//         b = temp;
-//         return new int[]{a, b};
-//     }
+    public static int[] change(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        return new int[]{a, b};
+    }
 
-//     public static void main(String[] args) {
-//         int a = 23;
-//         int b = 56;
-//         int[] swap = change(a, b);
-//         System.out.println(swap[0]);
-//         System.out.println(swap[1]);
-//     }
-// }
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 56;
+        int[] swap = change(a, b);
+        System.out.println(swap[0]);
+        System.out.println(swap[1]);
+    }
+}
 
 // basic of function
 
-public class functionsq{
-    public static void printhelloworld(){
-        System.out.println("hello world ");
-        System.out.println("Hello world");
-    }
+// public class functionsq{
+//     public static void printhelloworld(){
+//         System.out.println("hello world ");
+//         System.out.println("Hello world");
+//     }
 
-    public static void main (String args[]){
-        printhelloworld();
+//     public static void main (String args[]){
+//         printhelloworld();
 
-    }
-
-}
+//     }
+// }
 
 // FInd factorial of number 
 
