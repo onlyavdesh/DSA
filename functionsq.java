e3969218573a3a3eec606f123731ac22ab243a54
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2025-09-27 12:05:40 +0530

    git commit -m "This is my first commit on git"

--- functionsq.java
+++ functionsq.java
@@ -1,20 +1,38 @@
-public class functionsq {
+// public class functionsq {
 
-    public static int[] change(int a, int b) {
-        int temp = a;
-        a = b;
-        b = temp;
-        return new int[]{a, b};
+//     public static int[] change(int a, int b) {
+//         int temp = a;
+//         a = b;
+//         b = temp;
+//         return new int[]{a, b};
+//     }
+
+//     public static void main(String[] args) {
+//         int a = 23;
+//         int b = 56;
+//         int[] swap = change(a, b);
+//         System.out.println(swap[0]);
+//         System.out.println(swap[1]);
+//     }
+// }
+
+// basic of function
+
+public class functionsq{
+    public static void printhelloworld(){
+        System.out.println("hello world ");
+        System.out.println("Hello world");
     }
 
-    public static void main(String[] args) {
-        int a = 23;
-        int b = 56;
-        int[] swap = change(a, b);
-        System.out.println(swap[0]);
-        System.out.println(swap[1]);
+    public static void main (String args[]){
+        printhelloworld();
+
     }
+
 }
+
+// FInd factorial of number 
+
 
 
 
