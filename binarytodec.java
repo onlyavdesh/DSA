f57b93d1649ee6da761602bfabbbd2b76a19ea57
Avdesh Chaudhary <chaudharyavdesh075@gmail.com>
2025-09-27 23:45:37 +0530

    Here is new code

--- binarytodec.java
+++ binarytodec.java
@@ -1,20 +1,20 @@
-import java.util.*;
-public class binarytodec {
-    public static void bintodec(int binnum){
-        int pow =0;
-        int decnum = 0;
 
-        while(binnum>0){
-            int lastdigit = binnum % 10;
-            decnum = decnum + (lastdigit * (int)Math.pow(2,pow));
-            pow++;
-            binnum = binnum/10;
-        } 
-        System.out.println("Your last digit" + decnum);
-    }
+// public class binarytodec {
+//     public static void bintodec(int binnum){
+//         int pow =0;
+//         int decnum = 0;
+
+//         while(binnum>0){
+//             int lastdigit = binnum % 10;
+//             decnum = decnum + (lastdigit * (int)Math.pow(2,pow));
+//             pow++;
+//             binnum = binnum/10;
+//         } 
+//         System.out.println("Your last digit" + decnum);
+//     }
     
- public static void main( String args[]){
-    bintodec(111);
-}
-}
+//  public static void main( String args[]){
+//     bintodec(111);
+// }
+// }
 
